fa9f392f36b120c5c5230fc1bfeaafe786d3e30c
BrunoMont18 <126253017+BrunoMont18@users.noreply.github.com>
2023-06-23 10:16:47 -0300

    q7.go

--- q7.go
+++ q7.go
@@ -14,7 +14,7 @@ func adicionarValorConta(conta *Conta, valor float64) {
 func main() {
 	conta := Conta{
 		Saldo:   100.0,
-		Titular: "João",
+		Titular: "Lucas",
 	}
 	adicionarValorConta(&conta, 50.0)
 	fmt.Println("Saldo da conta:", conta.Saldo) // Saída: Saldo da conta: 150.0
